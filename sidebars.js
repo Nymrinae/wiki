ea67e147f2093f648d079c483ad390d321177c6b
AguitaDeLloros <97903250+AguitaDeLloros@users.noreply.github.com>
2023-01-07 12:33:57 +0100

    act7.2-guides

--- sidebars.js
+++ sidebars.js
@@ -186,6 +186,7 @@ module.exports = {
             { type: 'doc', id: 'guides/raids/carno-raids', label: 'Beast King Carno', className: 'carno-icon' },
             { type: 'doc', id: 'guides/raids/kirollas-raids', label: 'Spirit King Kirollas', className: 'kirollas-icon' },
             { type: 'doc', id: 'guides/raids/belial-raids', label: 'Demon God Belial', className: 'belial-icon' },
+            { type: 'doc', id: 'guides/raids/paimon', label: 'Evil Overlord Paimon', className: 'paimon-icon' }
           ]
         },
         {
@@ -193,7 +194,8 @@ module.exports = {
           label: 'Quests',
           className: 'questl-icon',
           items: [
-            { type: 'doc', id: 'guides/quests/act-7.1-questline', label: 'Act7 Questline', className:'mainquest-icon' },
+            { type: 'doc', id: 'guides/quests/act-7.1-questline', label: 'Act 7.1 Questline', className:'mainquest-icon' },
+            { type: 'doc', id: 'guides/quests/act-7.2-questline', label: 'Act 7.2 Questline', className:'mainquest-icon' },
             { type: 'doc', id: 'guides/quests/act-7.1-daily-quests', label: 'Act7 Daily Quests', className:'dailyquest-icon' },
           ]
         },
@@ -201,9 +203,9 @@ module.exports = {
         { type: 'doc', id: 'guides/act7-equipment', label: 'Act7 Equipment Crafting', className: 'a7craft-icon' },
         { type: 'doc', id: 'release-reworks/equipment-act71-rework', label: 'Act7 Equipment Rework', className: 'equi-icon' },
         { type: 'doc', id: 'guides/features/act7-achievements', label: 'Act7 Achievements', className: 'a7achievements-icon' },
-        { type: 'doc', id: 'release-reworks/act7-sp-changelog', label: 'Act7 SP Balance', className: 'act7sps-icon' },
         { type: 'doc', id: 'information/runes', label: 'Runes ', className: 'runes-icon' },
         { type: 'doc', id: 'information/tattoos', label: 'Tattoos', className: 'tattoos-icon' },
+        { type: 'doc', id: 'guides/features/celestialS', label: 'Celestial Spire', className: 'celestialS-icon' },
       ]
     }
   ]
